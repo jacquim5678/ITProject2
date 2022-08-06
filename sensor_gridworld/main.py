72a8ec294e51a38a60b9b1917dc12c24660784d2
jacquim5678 <jacquim5678@gmail.com>
2022-08-06 19:08:18 +1000

    grid colours working

--- sensor_gridworld/main.py
+++ sensor_gridworld/main.py
@@ -1,3 +1,4 @@
+import random
 from cgitb import reset
 import gym
 import seaborn as sns
@@ -18,14 +19,23 @@ class SensorGridWorld(gym.Env):
               self.ENEMY = 1
 
        def reset(self):
-              self.grid = np.random.randint(100, size=(1048,1048))
+              # 1 grid square = 1m2
+              self.grid = np.zeros((1048,1048))
+              # High Value Target
+              self.grid[509:539,509:539] = 75
+              numDrone = self.DRONES
+              for each in range (0,numDrone):
+                     temp1 = random.randint(0,1048)
+                     temp2 = random.randint(0, 1048)
+                     self.grid[temp1, temp2] = 200
+              self.grid[25:123,34:150] = 200
               temp = self.grid
               return temp
 
        def render(self, mode='console'):
               print(self.grid)
               sns.set(rc={'figure.figsize':(30,30)})
-              sns.heatmap(self.grid, cbar=False, cbar_ax = False, square = True, yticklabels = False, xticklabels = False)
+              sns.heatmap(self.grid, cbar=False, cbar_ax = False, square = True, yticklabels = False, xticklabels = False, vmin=0, vmax=200, cmap="cubehelix")
               plt.tight_layout(pad=0)
               plt.show()
 
