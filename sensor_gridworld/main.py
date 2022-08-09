0c760510a9676cd72b7693107d8695f45a0ab29f
jacquim5678 <68936260+jacquim5678@users.noreply.github.com>
2022-08-10 09:37:25 +1000

    sensor grid colours

--- sensor_gridworld/main.py
+++ sensor_gridworld/main.py
@@ -27,15 +27,19 @@ class SensorGridWorld(gym.Env):
               for each in range (0,numDrone):
                      temp1 = random.randint(0,1048)
                      temp2 = random.randint(0, 1048)
-                     self.grid[temp1, temp2] = 200
-              self.grid[25:123,34:150] = 200
+                     self.grid[temp1 - 100:temp1 + 100, temp2 - 100:temp2 + 100] = 150
+                     self.grid[temp1 - 75:temp1 + 75, temp2 - 75:temp2 + 75] = 200
+                     self.grid[temp1 - 25:temp1 + 25, temp2 - 25:temp2 + 25] = 125
+                     self.grid[temp1, temp2] = 0
+                     # High Value Target
+              self.grid[509:539, 509:539] = 75
               temp = self.grid
               return temp
 
        def render(self, mode='console'):
               print(self.grid)
               sns.set(rc={'figure.figsize':(30,30)})
-              sns.heatmap(self.grid, cbar=False, cbar_ax = False, square = True, yticklabels = False, xticklabels = False, vmin=0, vmax=200, cmap="cubehelix")
+              sns.heatmap(self.grid, cbar=False, cbar_ax = False, square = True, yticklabels = False, xticklabels = False, vmin=0, vmax=250, cmap="cubehelix")
               plt.tight_layout(pad=0)
               plt.show()
 
